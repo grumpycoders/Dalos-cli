fe1402c1e8e333970c5568527dde0e821671fcb7
Pixel <pixel@nobis-crew.org>
2012-04-01 12:43:33 -0700

    Proof of concept.

--- src/Dalos-cli.cc
+++ src/Dalos-cli.cc
@@ -1,7 +1,13 @@
 #include <Main.h>
+#include <TaskMan.h>
+#include <LuaTask.h>
 
 using namespace Balau;
 
 void MainTask::Do() {
     Printer::log(M_STATUS, "Dalos-cli starting");
+
+    LuaMainTask * luaMainTask = createTask(new LuaMainTask);
+    LuaExecString luaExecString("print 'foo'");
+    luaExecString.exec(luaMainTask);
 }
